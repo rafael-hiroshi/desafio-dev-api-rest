17879d41e309f3e1ebb6a1309c6a77fd0116138a
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-12-10 21:25:08 -0300

    Preparando testes e properties de teste

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO pessoas (cpf, data_nascimento, nome) VALUES('88370628001', '2000-09-12', 'Rafael Dev');
+INSERT INTO pessoas (cpf, data_nascimento, nome) VALUES('10444233660', '2000-11-03', 'Developer Test');
