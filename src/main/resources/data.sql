3d430d8e2e2f92ea24b6f3d3a6cb3932fdb3be44
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-12-12 21:35:34 -0300

    Incluindo dados de teste

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
 INSERT INTO pessoas (cpf, data_nascimento, nome) VALUES('88370628001', '2000-09-12', 'Rafael Dev');
 INSERT INTO pessoas (cpf, data_nascimento, nome) VALUES('10444233660', '2000-11-03', 'Developer Test');
+
+INSERT INTO contas (data_criacao, flag_ativo, limite_saque_diario, saldo, tipo_conta, id_pessoa) VALUES('2021-12-13', 1, 1000, 16000, 2, 1);
+INSERT INTO contas (data_criacao, flag_ativo, limite_saque_diario, saldo, tipo_conta, id_pessoa) VALUES('2021-12-13', 1, 1000, 2500, 1, 1);
+INSERT INTO contas (data_criacao, flag_ativo, limite_saque_diario, saldo, tipo_conta, id_pessoa) VALUES('2021-12-13', 1, 1000, 4000, 1, 2);
+INSERT INTO contas (data_criacao, flag_ativo, limite_saque_diario, saldo, tipo_conta, id_pessoa) VALUES('2021-12-13', 1, 1000, 0, 1, 2);
