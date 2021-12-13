8c787ce66e8e9a89bd0a3c6f710f18774c496add
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-12-12 22:29:08 -0300

    Teste de rota para depositar

--- src/test/java/br/com/rafael/api/controller/ContaControllerTest.java
+++ src/test/java/br/com/rafael/api/controller/ContaControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -26,60 +27,75 @@ public class ContaControllerTest {
     @Autowired
     private MockMvc mockMvc;
     private static URI rotaCadastrar;
+    private static URI rotaDepositar;
     private String payload;
 
     @BeforeAll
     public static void setup() throws URISyntaxException {
         rotaCadastrar = new URI("/conta/cadastrar");
+        rotaDepositar = new URI("/conta/depositar");
     }
 
     @Test
     public void testCriarContaValidaDeveRetornarStatusCode201() throws Exception {
         payload = new JSONObject()
-                .put("idPessoa", "1")
-                .put("tipoConta", "1")
+                .put("idPessoa", 1)
+                .put("tipoConta", 1)
                 .toString();
 
-        mockMvc
-                .perform(MockMvcRequestBuilders
-                        .post(rotaCadastrar)
-                        .content(payload)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers
-                        .status()
-                        .is(201));
+        doRequest(rotaCadastrar, 201);
     }
 
     @Test
     public void testCriarContaComPessoaNaoCadastradaDeveRetornarStatusCode404() throws Exception {
         payload = new JSONObject()
-            .put("idPessoa", "999")
-            .put("tipoConta", "1")
+            .put("idPessoa", 999)
+            .put("tipoConta", 1)
             .toString();
 
-        mockMvc
-                .perform(MockMvcRequestBuilders
-                        .post(rotaCadastrar)
-                        .content(payload)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers
-                        .status()
-                        .is(404));
+        doRequest(rotaCadastrar, 404);
     }
 
     @Test
     public void testCriarContaComTipoDeContaInvalidoDeveRetornarStatusCode400() throws Exception {
         payload = new JSONObject()
-                .put("idPessoa", "1")
-                .put("tipoConta", "0")
+                .put("idPessoa", 1)
+                .put("tipoConta", 0)
                 .toString();
 
+        doRequest(rotaCadastrar, 400);
+    }
+
+    @Test
+    public void testDepositarComSaldoInsuficienteDeveRetornarStatusCode400() throws Exception {
+        payload = new JSONObject()
+                .put("idContaOrigem", 4)
+                .put("idContaDestino", 1)
+                .put("valor", new BigDecimal(200))
+                .toString();
+
+        doRequest(rotaDepositar, 400);
+    }
+
+    @Test
+    public void testDepositarComSaldoSuficienteDeveRetornarStatusCode200() throws Exception {
+        payload = new JSONObject()
+                .put("idContaOrigem", 1)
+                .put("idContaDestino", 4)
+                .put("valor", new BigDecimal(1000))
+                .toString();
+
+        doRequest(rotaDepositar, 200);
+    }
+
+    private void doRequest(URI rota, int statusCode) throws Exception {
         mockMvc
                 .perform(MockMvcRequestBuilders
-                        .post(rotaCadastrar)
+                        .post(rota)
                         .content(payload)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers
                         .status()
-                        .is(400));
-    }}
+                        .is(statusCode));
+    }
+}
